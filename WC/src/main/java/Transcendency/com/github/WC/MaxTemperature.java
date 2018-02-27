ca7858980d3af3b40be68410f5a9f9028ebf4ba3
Leslie Li <jeryliketom@gmail.com>
2018-02-27 11:15:18 -0500

    add Combiner

--- WC/src/main/java/Transcendency/com/github/WC/MaxTemperature.java
+++ WC/src/main/java/Transcendency/com/github/WC/MaxTemperature.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MaxTemperature {
 	public static void main(String[] args) throws Exception{
@@ -32,17 +32,32 @@ public class MaxTemperature {
 //			e.printStackTrace();
 //		}
 		
+		// OLD MAP REDUCE API WITH COMBINER
+		JobConf conf = new JobConf(MaxTemperature.class); conf.setJobName("Max temperature");
+		FileInputFormat.addInputPath(conf, new Path(args[0])); 
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		conf.setMapperClass(MaxTemperatureMapper.class);
+		conf.setCombinerClass(MaxTemperatureReducer.class);
+		conf.setReducerClass(MaxTemperatureReducer.class);
+		conf.setOutputKeyClass(Text.class); 
+		conf.setOutputValueClass(IntWritable.class);
+		try {
+			JobClient.runJob(conf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		// NEW MAP REDUCE API
-		Job job = new Job();
-		job.setJarByClass(MaxTemperature.class);
-		FileInputFormat.addInputPath(job, new Path(args[0])); 
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
-		job.setMapperClass(NewMaxTemperatureMapper.class);
-		job.setReducerClass(NewMaxTemperatureReducer.class);
-		
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+//		Job job = new Job();
+//		job.setJarByClass(MaxTemperature.class);
+//		FileInputFormat.addInputPath(job, new Path(args[0])); 
+//		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+//		
+//		job.setMapperClass(NewMaxTemperatureMapper.class);
+//		job.setReducerClass(NewMaxTemperatureReducer.class);
+//		
+//		job.setOutputKeyClass(Text.class);
+//		job.setOutputValueClass(IntWritable.class);
+//		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
